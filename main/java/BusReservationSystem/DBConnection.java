9d046945a33c5b61ec9e9fb6565e2abbc4063453
Aginas B <138473118+aginasb2109@users.noreply.github.com>
2025-07-04 23:44:58 +0530

    Update DBConnection.java

--- main/java/BusReservationSystem/DBConnection.java
+++ main/java/BusReservationSystem/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String url="jdbc:mysql://localhost:3306/BusReserve";
     private static final String user="root";
-    private static final String password="Aginas@2109";
+    private static final String password="Your password";
 
 
     public static Connection getConnection() throws SQLException {
